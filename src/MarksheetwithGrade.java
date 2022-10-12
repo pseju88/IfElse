e34ce6000102c8ae2c30b8e805b42187e9907ba0
Sejal patel <sejarp88@gmail.com>
2022-10-12 10:10:11 +0100

    MarksheetwithGrade commit

---
+++ src/MarksheetwithGrade.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class MarksheetwithGrade {
+    static String name;
+    static int RollNo;
+    static double total;
+    static double percentage;
+    static double Accontancy, Econonic, English;
+
+    public static void main(String[] args) {
+
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter name");
+            name = scanner.next();
+            System.out.println("Enter RollNO");
+            RollNo = scanner.nextInt();
+            System.out.println("Enter Accountancy Marks");
+            Accontancy = scanner.nextInt();
+            System.out.println("Enter Economic Marks");
+            Econonic = scanner.nextInt();
+            System.out.println("Enter English Marks");
+            English = scanner.nextInt();
+            {
+                double total = (Accontancy + Econonic + English);
+                double percentage = total / 300.0 * 100;
+                System.out.println(name + "Rollnumber=" + RollNo + "Total marks =" + total + " =" + " percentage = " + percentage + "%");
+
+                if (percentage >= 80) {
+                    System.out.println(" you Passed with A+ Grade");
+                } else if (percentage >= 60) {
+                    System.out.println("you Passed with A Grade");
+                } else if (percentage >= 50) {
+                    System.out.println("you Passed with B Grade");
+                } else if (percentage >= 35) {
+                    System.out.println("you Passed with C Grade");
+                }
+            }
+        }
+    }
+}
+
